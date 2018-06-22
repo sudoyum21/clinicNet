05d60fb320f8044c269eeaa093a17232b146c640
sudoyum21 <ba.samson.lam@gmail.com>
2018-06-21 21:26:38 -0400

    seperating admin and patient

---
+++ 1.0/admin/FirstFloor.ModernUI.App/SingletonUser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clinic.admin
+{
+    public static class SingletonUser 
+    {
+        public static string user = "";
+        public static SampleFormViewModel form;
+        public static SampleFormViewModel formOriginal;
+        public static void ValidateForm(SampleFormViewModel.FormModel f)
+        {
+            form.AddressInvalid = form.Address != formOriginal.Address;
+            form.AddressInvalid = form.Address != formOriginal.Address;
+            form.AddressInvalid = form.Address != formOriginal.Address;
+            form.AddressInvalid = form.Address != formOriginal.Address;
+            form.AddressInvalid = form.Address != formOriginal.Address;
+            form.AddressInvalid = form.Address != formOriginal.Address;
+            form.AddressInvalid = form.Address != formOriginal.Address;
+            form.AddressInvalid = form.Address != formOriginal.Address;
+            form.AddressInvalid = form.Address != formOriginal.Address;
+            form.AddressInvalid = form.Address != formOriginal.Address;
+            form.SaveForm();
+        }
+    } 
+}
---
+++ 1.0/patient/FirstFloor.ModernUI.App/Pages/Introduction.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace clinic.admin.Pages
+{
+    /// <summary>
+    /// Interaction logic for Introduction.xaml
+    /// </summary>
+    public partial class Introduction : UserControl
+    {
+        public Introduction()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangeUser_Click(object sender, RoutedEventArgs e)
+        {
+            //IInputElement target = NavigationHelper.FindFrame("_top", this);
+            //NavigationCommands.GoToPage.Execute("/Pages/BasicPage1.xaml", target);
+        }
+    }
+}
